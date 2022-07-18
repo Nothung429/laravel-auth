fdd2cf731b7b610f8e180a3b85d2af3ff6088de6
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-18 18:31:49 +0200

    add login, fixed route & vue

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .js('resources/js/front_office.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/front_office.scss', 'public/css');
